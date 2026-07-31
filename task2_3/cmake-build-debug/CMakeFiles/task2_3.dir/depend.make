22073a1d7184f856614b7ab9d5dbc0506075c365
Topakov <topa990048@mail.ru>
2026-07-31 21:24:35 +0700

    pupupu

--- task2_3/cmake-build-debug/CMakeFiles/task2_3.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for task2_3.
-# This may be replaced when dependencies are built.
--- task3_1/src/main.c
+++ task3_1/src/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+void print_binary(int mode) {
+
+    for (int i = 8; i >= 0; i--) {
+
+        if (mode & (1 << i)) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+        if (i == 6 || i == 3) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+
+
+
+
+
 int main(void) {
-    printf("Hello, World!\n");
+
+int mode = 0146;
+    print_binary(mode);
     return 0;
 }
