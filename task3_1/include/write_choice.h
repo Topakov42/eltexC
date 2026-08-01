cd202237d9d57d67e5cd9d38a16166ef36732d6a
Topakov <topa990048@mail.ru>
2026-08-01 18:13:14 +0700

    add fun: write_choice

---
+++ task3_1/include/write_choice.h
@@ -0,0 +1,8 @@
+//
+// Created by topa on 01.08.2026.
+//
+
+#ifndef TASK3_1_WRITE_CHOICE_H
+#define TASK3_1_WRITE_CHOICE_H
+int write_choice();
+#endif //TASK3_1_WRITE_CHOICE_H
--- task3_1/src/main.c
+++ task3_1/src/main.c
@@ -3,9 +3,29 @@
 #include "../include/parse_rwx.h"
 #include "../include/read_file_rights.h"
 #include "../include/print_letters.h"
+#include "../include/write_choice.h"
 
 
 int main(void) {
+
+
+    printf("Выбирите операцию : \n");
+    printf("1) Ввести права доступа, для вывода его в бинарном виде : \n");
+    printf("2) Указать имя файла, для вывода его прав доступа\nв буквенном, цифровом и битовом виде : \n");
+    int choice = write_choice();
+
+    switch (choice) {
+        case 1:
+            printf("первый варик");
+            break;
+        case 2:
+            printf("Второй варик");
+            break;
+            default:
+            printf("Что то не понятное");
+            break;
+    }
+
 
 
 
